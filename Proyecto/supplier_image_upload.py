c4e550329cbf61edf0a4548f26af34e5bc838aa8
Audionoob <danielmvazquez.audio@gmail.com>
2023-02-27 19:25:30 +0100

    +run.py

--- Proyecto/supplier_image_upload.py
+++ Proyecto/supplier_image_upload.py
@@ -11,19 +11,19 @@ files = os.listdir(FOLDER)
 def it_folder():
     for fname in files:
         with open((FOLDER + "/" + fname), "rb") as data_file:
-            process(data_file)
+            post_imgs(data_file)
 
 
 # Where the processment occurs
 def process(data_file):
     # TODO Procesar imagenes
-    post_fback()
+    post_imgs()
     pass
 
 
 # Post images to the webservice
-def post_fback(data):
-    requests.post("http://????????/feedback/", data)
+def post_imgs(data):
+    requests.post("http://????????/feedback/", files={'file': data})
 
 
 if __name__ == "__main__":
